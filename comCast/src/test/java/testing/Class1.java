41488f1044e1c329516b84fd50834ef037092fdb
ELCOT <ELCOT@192.168.100.7>
2022-05-10 07:51:47 -0700

    committing1

---
+++ comCast/src/test/java/testing/Class1.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hai");
+System.out.println("git practice");
+	}
+
+}
