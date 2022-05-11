6d92185738227b3b901c6525b348a109c2b12a35
ELCOT <ELCOT@Lenovo>
2022-05-11 02:55:03 -0700

    vjjh

--- comCast/src/test/java/testing/Class2.java
+++ comCast/src/test/java/testing/Class2.java
@@ -11,6 +11,8 @@ System.out.println("hello");
 System.out.println("haiii");
 System.out.println("hhh");
 System.out.println("git practice");
+System.out.println("hhh");
+System.out.println("git practice");
 System.out.println("git practice2");
 System.out.println("hello");
 
